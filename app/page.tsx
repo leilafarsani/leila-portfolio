3f0a195ea2dedbd42b9cb8f3282d2c1651e2dca8
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-08-12 01:47:08 +0100

    creating Hero page as my homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 
+import Hero from "./scenes/Hero";
 export default function Home() {
   return (
     <main>
-
+      <Hero />
     </main>
   );
 }
---
+++ app/scenes/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>H</div>
+  )
+}
+
+export default Hero

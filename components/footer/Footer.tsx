76b239f1b5f3295f721eaa70d0e4bf02e623a172
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-08-13 22:40:06 +0100

    Footer Added

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-300 py-4 text-center">
+      {" "}
+      <p className="text-gray-600">
+        &copy;{new Date().getFullYear()} Leila FARSANI. All rights reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Footer

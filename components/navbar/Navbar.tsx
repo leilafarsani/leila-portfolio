566a4a6a26f85ef7bd417f9a9d6ac33cc60fe6e8
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-08-15 19:51:59 +0100

    navbar created!

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { RxHamburgerMenu } from "react-icons/rx";
+import Button from "../buttons/Button";
+import {useState } from "react";
+import DropMenu from "./DropMenu";
+import { GrFormClose } from "react-icons/gr";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const [drop, setDrop] = useState(false);
+  const router = useRouter();
+
+  return (
+    <header>
+      <div className="flex items-center justify-between px-6 py-6 cursor-default">
+        <Link className="uppercase text-[1.5rem] z-50 ml-4" href="/">
+          Leila <span className="text-gray-400">Portfolio</span>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Button
+            icon={BsFillArrowRightCircleFill}
+            label="Contact"
+            onClick={() => router.push("/contact")}
+          />
+
+          <div className="rounded-full bg-gray-300 p-2 z-[999999]">
+            {drop ? (
+              <GrFormClose
+                className="text-[1.5rem] cursor-pointer"
+                onClick={() => setDrop(!drop)}
+              />
+            ) : (
+              <RxHamburgerMenu
+                className="text-[1.5rem] cursor-pointer"
+                onClick={() => setDrop(!drop)}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+
+      <AnimatePresence>
+        {drop && <DropMenu drop={drop} setDrop={setDrop} />}
+      </AnimatePresence>
+    </header>
+  );
+}

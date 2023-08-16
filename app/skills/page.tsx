346427b8c51a60bebbba64747b0f7cea19f309bc
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-08-16 17:04:21 +0100

    images added to cards

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -6,22 +6,105 @@ import SkillsCard from "./SkillsCard";
 
 const card = [
   {
-    image: "",
+    image: "/html.png",
     ImgTitle: "",
     name: "S1 ",
-    description:
-      "",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/css.jpg",
+    ImgTitle: "no title",
+    name: "S2",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/js.png",
+    ImgTitle: "",
+    name: "S3 ",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/ts.jpg",
+    ImgTitle: "",
+    name: "S4 ",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/react.png",
+    ImgTitle: "",
+    name: "S5",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/next2.jpg",
+    ImgTitle: "",
+    name: "S1 ",
+    description: "",
     onClick: "",
   },
   {
     image: "/node.png",
-    ImgTitle: "no title",
-    name: "S2",
-    description:
-      "",
+    ImgTitle: "",
+    name: "S6 ",
+    description: "",
     onClick: "",
   },
-]
+  {
+    image: "/sql.jpg",
+    ImgTitle: "",
+    name: "S7",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/mysql.png",
+    ImgTitle: "",
+    name: "S8",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/psql.jpg",
+    ImgTitle: "",
+    name: "S9",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/mongodb.jpg",
+    ImgTitle: "",
+    name: "S7",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "/prisma.png",
+    ImgTitle: "",
+    name: "S7",
+    description: "",
+    onClick: "",
+  },
+
+  {
+    image: "/git.png",
+    ImgTitle: "",
+    name: "S7",
+    description: "",
+    onClick: "",
+  },
+  {
+    image: "aws.png",
+    ImgTitle: "",
+    name: "S7",
+    description: "",
+    onClick: "",
+  }
+];
 
 export default function SkillsPage() {
   const router = useRouter();

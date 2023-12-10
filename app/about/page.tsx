10d5bb5da2f944dbc9a5a6fc23040e4f09f47ac4
Leila Y-Farsani <108967493+leilafarsani@users.noreply.github.com>
2023-12-10 22:18:59 +0000

    updated the about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,42 +8,42 @@ export default function AboutPage() {
       <div className="container mx-auto text-center">
         <h1 className="text-3xl md:text-6xl font-bold mb-4">About Me</h1>
         <p className="text-l mb-12">
-          Passionate about the realm of software development, and armed with a
-          Mathematics PhD. I bring a skillset enriched in research, data
+          Passionate Data Scientist, armed with a
+          Mathematics PhD, Skilled in software development, and teaching diverse groups.
+          I bring a skillset enriched in research, data
           modeling, and coding. My knack for creative problem-solving and
           step-by-step thinking sets me apart. I'm on a constant journey of
-          learning, eager to create fresh and exciting web solutions. A true
+          learning, and sharing  what I learnt with others. A true
           team player, I value effective communication and camaraderie. I can't
-          wait to dive in and craft web experiences that leave an impact!
-        </p>
+          wait to dive in and craft web experiences that leave an impact! </p>
 
         <div className="mb-12">
           <SectionCard
             title="Education"
-            content={`Full Stack Development Bootcamp, CodeYourFuture, London, UK\nDoctorate in Mathematics, University of Sussex, Brighton, UK\nFirst Class Honours , Bachelor's Degree Accounting, Isfahan, Iran\nMathematics , First Class Honours Diploma , Isfahan, Iran`}
+            content={`Full Stack Development and Data Science Bootcamp, CodeYourFuture, London, UK\nDoctorate in Mathematics, University of Sussex, Brighton, UK\nFirst Class Honours , Bachelor's Degree Accounting, Isfahan, Iran\nMathematics , First Class Honours Diploma , Isfahan, Iran`}
           />
         </div>
 
         <div className="mb-12">
           <SectionCard
             title="Experience"
-            content={`Tech Volunteering, CodeYourFuture, London\nMathematics Teaching, Private Lessons, Hybrid\nExamination Reader, Scribe, and Invigilator, University of Sussex, Brighton\nAssociate Tutor, University of Sussex, Brighton`}
+            content={`Data Scientist, WeFindFlats, London, UK\nTech Volunteering, CodeYourFuture, London\nMathematics Teaching, Private Lessons, Hybrid\nExamination Reader, Scribe, and Invigilator, University of Sussex, Brighton\nAssociate Tutor, University of Sussex, Brighton`}
           />
         </div>
         <div className="mb-12">
           <SectionCard
             title="Hobbies"
-            content={`Dancing\nPhotography\nSolving puzzles\nPlaying the Tar instrument\nExploring Psychology and philosophy`}
+            content={`Photography\nSolving puzzles\nPlaying the Tar instrument\nExploring Psychology and philosophy`}
           />
         </div>
 
-        <Link
+        {/* <Link
           href="https://drive.google.com/file/d/1ezLWOoP32tMzuJ2Y1iOGiw6tMin_yyVU/view"
           className=" px-6 py-3 rounded-md hover:bg-grey-700 transition duration-300 inline-flex items-center bg-gray-200 text-gray-600 font-semibold"
           target="_blank"
         >
           Download CV <FaDownload className="ml-3" />
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
